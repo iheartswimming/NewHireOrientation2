b65a067fef66fddb76c4502ab0044f0d8c56e340
iheartswimming <109818045+iheartswimming@users.noreply.github.com>
2025-08-17 11:39:37 -0400

    Add files via upload

---
+++ story_content/triggers.js
@@ -0,0 +1,51 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6kyw3P2pDep":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+window.Script1 = function()
+{
+  player.once(() => {
+const target = object('6n8K6AySGGn');
+const duration = 750;
+const easing = 'ease-out';
+const id = '5X5i1pBhGCk';
+const shakeAmount = 5;
+const delay = 4729;
+addToTimeline(
+target.animate([
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `${shakeAmount}px 0` },
+{ translate: '0 0' },
+{ translate: `-${shakeAmount}px 0` },
+{ translate: '0 0' }
+],
+  { fill: 'forwards', delay, duration, easing }
+), id
+);
+});
+}
+
+};
